a25dbd58387657358f9de7a3e7533f0f93ad7bd4
pavelp21 <pavelvp2182@yandex.ru>
2020-02-13 13:50:58 +0500

    Page is stylezed

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,8 +1,24 @@
 //@prepros-prepend jquery-2.1.1.min.js
+var isIE = false || !!document.documentMode;
 
-$(document).ready(function() {
+if (isIE) {
+    var head = document.getElementsByTagName("head")[0];
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "css/ie.min.css";
+    head.appendChild(link);
+}
+$(document).ready(function () {
+    /* Якорь */
+    $("a[href='#prices']").click(function (h) {
+        h.preventDefault();
+        var f = $(this).attr("href"),
+            g = $(f).offset().top;
+        $("body,html").animate({
+            scrollTop: g
+        }, 1500)
+    });
 
 
-
-/*Конец документа*/
+    /*Конец документа*/
 });
